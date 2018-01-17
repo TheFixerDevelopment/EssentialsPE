9b791481eaa502a15109be17e353b2d07b4b01ca
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-01-17 18:06:39 +0000

    Update TPAHere.php

--- src/EssentialsPE/Commands/Teleport/TPAHere.php
+++ src/EssentialsPE/Commands/Teleport/TPAHere.php
@@ -42,7 +42,7 @@ class TPAHere extends BaseCommand{
             return false;
         }
         $this->getAPI()->requestTPHere($sender, $player);
-        $player->sendMessage(TextFormat::AQUA . $sender->getName() . TextFormat::GREEN . " §dwants you to teleport to him, please use:\n§5/tpaccept §dto accepts the request\n§5/tpdeny §dto decline the invitation");
+        $player->sendMessage(TextFormat::AQUA . $sender->getName() . TextFormat::GREEN . " §dwants you to teleport to them, please use:\n§5/tpaccept §dto accepts the request\n§5/tpdeny §dto decline the invitation");
         $sender->sendMessage(TextFormat::GREEN . "§dTeleport request sent to§3 " . $player->getDisplayName(). "§dsuccesfully!");
         return true;
     }
